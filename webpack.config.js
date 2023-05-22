316dd90379df20f615a9be435459f5a75a46bde1
Aq4rius <maksbielecki6@gmail.com>
2023-05-22 23:40:47 +0200

    Edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,28 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+              publicPath: (url) => `../fonts/${url}`,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ico|png|svg|jpg|jpeg|gif|svg|webp|tiff)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
+      },
+    ],
+  },
 };
